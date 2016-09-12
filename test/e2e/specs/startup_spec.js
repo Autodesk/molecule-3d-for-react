de562afe3227bec25759e50c0181b7572a26323f
Justin McCandless <justinjmccandless@gmail.com>
2016-09-12 13:26:40 -0700

    e2e setup fixture that sets window size

--- test/e2e/specs/startup_spec.js
+++ test/e2e/specs/startup_spec.js
@@ -1,6 +1,8 @@
+const setup = require('../fixtures/setup');
+
 module.exports = {
   'Startup Test': (browser) => {
-    browser
+    setup(browser)
       .url(browser.launchUrl)
       .waitForElementVisible('.nbmolviz3d canvas', 1000, 'nbmolviz3d canvas element appears')
       .end();
