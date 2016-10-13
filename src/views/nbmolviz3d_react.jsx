da164f7d0ab0c157cb658213ad97669723b1ebcb
Justin McCandless <justinjmccandless@gmail.com>
2016-10-13 12:38:21 -0700

    Accept either an object or an Immutable.js map for modelData (for immutable object comparison optimization)

--- src/views/nbmolviz3d_react.jsx
+++ src/views/nbmolviz3d_react.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Map as IMap } from 'immutable';
 import MolWidget3DModel from '../models/mol_widget_3d_model';
 import MolWidget3DView from '../views/mol_widget_3d_view';
 
@@ -7,14 +8,24 @@ class Nbmolviz3dReact extends React.Component {
     this.renderNbmolviz();
   }
 
+  shouldComponentUpdate(nextProps) {
+    const changingModelData = IMap.isMap(nextProps.modelData) &&
+      (nextProps.modelData !== this.props.modelData);
+    const changingBG = nextProps.backgroundColor !== this.props.backgroundColor;
+
+    return changingModelData || changingBG;
+  }
+
   componentDidUpdate() {
     this.renderNbmolviz();
   }
 
   renderNbmolviz() {
-    if (this.props.modelData.atoms.length && this.props.modelData.bonds.length) {
+    const modelData = this.props.modelData.toJS();
+
+    if (modelData.atoms.length && modelData.bonds.length) {
       const model = new MolWidget3DModel({
-        model_data: this.props.modelData,
+        model_data: modelData,
         background_color: this.props.backgroundColor,
       });
       const view = new MolWidget3DView({
@@ -38,10 +49,10 @@ class Nbmolviz3dReact extends React.Component {
 
 Nbmolviz3dReact.propTypes = {
   backgroundColor: React.PropTypes.string,
-  modelData: React.PropTypes.shape({
-    atoms: React.PropTypes.array.isRequired,
-    bonds: React.PropTypes.array.isRequired,
-  }).isRequired,
+  modelData: React.PropTypes.oneOfType([
+    React.PropTypes.instanceOf(IMap),
+    React.PropTypes.object,
+  ]).isRequired,
 };
 
 export default Nbmolviz3dReact;
