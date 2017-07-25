58cac531703f7ba7b7605eb15a61afef65752f4f
Aaron Virshup <avirshup@gmail.com>
2017-07-25 12:28:05 -0700

    Arbitrary text labels

--- src/components/molecule_3d.jsx
+++ src/components/molecule_3d.jsx
@@ -22,9 +22,10 @@ class Molecule3d extends React.Component {
     selectedAtomIds: [],
     selectionType: selectionTypesConstants.ATOM,
     shapes: [],
+    labels: [],
     styles: {},
     width: '500px',
-  }
+  };
 
   static propTypes = {
     atomLabelsShown: React.PropTypes.bool,
@@ -49,9 +50,10 @@ class Molecule3d extends React.Component {
       selectionTypesConstants.CHAIN,
     ]),
     shapes: React.PropTypes.arrayOf(React.PropTypes.object),
+    labels: React.PropTypes.arrayOf(React.PropTypes.object),
     styles: React.PropTypes.objectOf(React.PropTypes.object),
     width: React.PropTypes.string,
-  }
+  };
 
   static isModelDataEmpty(modelData) {
     return modelData.atoms.length === 0 && modelData.bonds.length === 0;
@@ -84,6 +86,15 @@ class Molecule3d extends React.Component {
       if (shape.type) {
         glviewer[`add${shape.type}`](libUtils.getShapeSpec(shape));
       }
+    });
+  }
+
+  static render3dMolLabels(glviewer, labels) {
+    console.log(labels);
+    glviewer.removeAllLabels();
+    labels.forEach((label) => {
+      console.log(label);
+      glviewer.addLabel(label.text, label);
     });
   }
 
@@ -208,11 +219,8 @@ class Molecule3d extends React.Component {
       );
     });
 
-    if (!this.props.atomLabelsShown) {
-      glviewer.removeAllLabels();
-    }
-
     Molecule3d.render3dMolShapes(glviewer, this.props.shapes);
+    Molecule3d.render3dMolLabels(glviewer, this.props.labels);
     Molecule3d.render3dMolOrbital(glviewer, this.props.orbital);
 
     glviewer.setBackgroundColor(
